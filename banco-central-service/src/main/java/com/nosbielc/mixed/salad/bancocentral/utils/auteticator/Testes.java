007009245a30d16ee6e1b43a435123ace2b42d82
Cleibson Gomes <cleibson.gomes.silva@gmail.com>
2019-05-26 11:50:41 -0300

    Update Testes.java

--- banco-central-service/src/main/java/com/nosbielc/mixed/salad/bancocentral/utils/auteticator/Testes.java
+++ banco-central-service/src/main/java/com/nosbielc/mixed/salad/bancocentral/utils/auteticator/Testes.java
@@ -5,27 +5,23 @@ import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class testes {
+public class Testes {
 
-    private static final Logger log = LoggerFactory.getLogger(testes.class);
+    private static final Logger log = LoggerFactory.getLogger(Testes.class);
     private static Boolean isHabilitarLoop = Boolean.TRUE;
 
     public static void main(String[] args) throws InterruptedException {
         //Gerando autenticação
         GoogleAuthenticator gAuth = new GoogleAuthenticator();
         final GoogleAuthenticatorKey key = gAuth.createCredentials();
-        log.info("KeyVerification: {0}", key.getVerificationCode());
-        log.info("Key: {0}" , key.getKey());
-
-        //Checando token
-//        boolean isCodeValid = gAuth.authorize("NUWSNYXVSG67KUDI", 190631);
-//        log.info("Valido ? {0} ", isCodeValid);
+        log.info("KeyVerification: {}", key.getVerificationCode());
+        log.info("Key: {}", key.getKey());
 
         while (isHabilitarLoop) {
             int code = gAuth.getTotpPassword(key.getKey());
             boolean isCodeValidTest = gAuth.authorize(key.getKey(), code);
-            log.info("Codigo Gerado pelo app ? {0} " ,  code);
-            log.info("Valido ? {0} ", isCodeValidTest);
+            log.info("Codigo Gerado pelo app ? {} ", code);
+            log.info("Valido ? {} ", isCodeValidTest);
             Thread.sleep(90000);
         }
 

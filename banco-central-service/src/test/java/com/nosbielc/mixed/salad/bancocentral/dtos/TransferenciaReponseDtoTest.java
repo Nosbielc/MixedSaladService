0337ccb6e7dd4cbad700057c674f3c80eff6e2e6
Cleibson Gomes <cleibson.gomes.silva@gmail.com>
2019-05-26 13:16:27 -0300

    Update TransferenciaReponseDtoTest.java

--- banco-central-service/src/test/java/com/nosbielc/mixed/salad/bancocentral/dtos/TransferenciaReponseDtoTest.java
+++ banco-central-service/src/test/java/com/nosbielc/mixed/salad/bancocentral/dtos/TransferenciaReponseDtoTest.java
@@ -1,101 +1,170 @@
 package com.nosbielc.mixed.salad.bancocentral.dtos;
 
+import com.nosbielc.mixed.salad.bancocentral.entities.Banco;
+import com.nosbielc.mixed.salad.bancocentral.entities.Transferencia;
+import com.nosbielc.mixed.salad.bancocentral.enums.TransferenciaStatus;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Date;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 public class TransferenciaReponseDtoTest {
 
+    private static final String STR = "str";
+    private static final String STR_ATUALIZADO = "str-atualizado";
+    private static final Float VLR_TRANSFERENCIA = 1.00f;
+    private static final Float VLR_TRANSFERENCIA_ATUALIZADO = 1.50f;
+
+    private TransferenciaReponseDto transferenciaReponseDto;
+    private Transferencia transferencia;
+    private Banco bancoOrigem, bancoDestino;
+    private BancoDto bancoDtoOrigem, bancoDtoDestino;
+
+    @Before
+    public void initTest() {
+        bancoOrigem = new Banco(Long.MIN_VALUE, STR, STR, Boolean.TRUE);
+        bancoDestino = new Banco(Long.MIN_VALUE, STR, STR, Boolean.TRUE);
+        bancoDtoOrigem = new BancoDto(Long.MIN_VALUE, STR, STR, Boolean.TRUE);
+        bancoDtoDestino = new BancoDto(Long.MIN_VALUE, STR, STR, Boolean.TRUE);
+        transferencia =
+                new Transferencia(STR, VLR_TRANSFERENCIA, bancoOrigem
+                , STR, TransferenciaStatus.REQUESTED, bancoDestino, STR, new Date());
+        transferencia.setId(Long.MIN_VALUE);
+        transferenciaReponseDto = new TransferenciaReponseDto(transferencia);
+    }
+
+    @Test
+    public void testTransferenciaReponseDto () {
+        transferenciaReponseDto = new TransferenciaReponseDto();
+        assertNotNull(transferenciaReponseDto);
+    }
+
+    @Test
+    public void testTransferenciaReponseDtoComParametros() {
+        transferenciaReponseDto =
+                new TransferenciaReponseDto(Long.MIN_VALUE, STR, VLR_TRANSFERENCIA, new Date(),
+                        bancoDtoOrigem, STR, TransferenciaStatus.PENDING,
+                        bancoDtoDestino, STR);
+        assertNotNull(transferenciaReponseDto);
+    }
+
+    @Test
+    public void testToString() {
+        String toString = transferenciaReponseDto.toString();
+        assertEquals(
+                toString,
+                transferenciaReponseDto.toString()
+        );
+    }
+
     @Test
     public void getId() {
-        assertTrue(Boolean.TRUE);
+        assertTrue(Long.MIN_VALUE == transferenciaReponseDto.getId());
     }
 
     @Test
     public void setId() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setId(1L);
+        assertTrue(1L == transferenciaReponseDto.getId());
     }
 
     @Test
     public void getAutenticacao() {
-        assertTrue(Boolean.TRUE);
+        assertEquals(STR, transferenciaReponseDto.getAutenticacao());
     }
 
     @Test
     public void setAutenticacao() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setAutenticacao(STR_ATUALIZADO);
+        assertEquals(STR_ATUALIZADO, transferenciaReponseDto.getAutenticacao());
     }
 
     @Test
     public void getValorTransferencia() {
-        assertTrue(Boolean.TRUE);
+        assertTrue(VLR_TRANSFERENCIA == transferenciaReponseDto.getValorTransferencia());
     }
 
     @Test
     public void setValorTransferencia() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setValorTransferencia(VLR_TRANSFERENCIA_ATUALIZADO);
+        assertTrue(VLR_TRANSFERENCIA_ATUALIZADO == transferenciaReponseDto.getValorTransferencia());
     }
 
     @Test
     public void getDateTimeTransferencia() {
-        assertTrue(Boolean.TRUE);
+        assertNotNull(transferenciaReponseDto.getDateTimeTransferencia());
     }
 
     @Test
     public void setDateTimeTransferencia() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setDateTimeTransferencia(null);
+        assertNull(transferenciaReponseDto.getDateTimeTransferencia());
+        transferenciaReponseDto.setDateTimeTransferencia(new Date());
+        assertNotNull(transferenciaReponseDto.getDateTimeTransferencia());
     }
 
     @Test
     public void getBancoOrigem() {
-        assertTrue(Boolean.TRUE);
+        assertNotNull(transferenciaReponseDto.getBancoOrigem());
     }
 
     @Test
     public void setBancoOrigem() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setBancoOrigem(null);
+        assertNull(transferenciaReponseDto.getBancoOrigem());
+        transferenciaReponseDto.setBancoOrigem(bancoDtoOrigem);
+        assertNotNull(transferenciaReponseDto.getBancoOrigem());
     }
 
     @Test
     public void getContaOrigem() {
-        assertTrue(Boolean.TRUE);
+        assertEquals(STR, transferenciaReponseDto.getContaOrigem());
     }
 
     @Test
     public void setContaOrigem() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setContaOrigem(STR_ATUALIZADO);
+        assertEquals(STR_ATUALIZADO, transferenciaReponseDto.getContaOrigem());
     }
 
     @Test
     public void getTransferenciaStatus() {
-        assertTrue(Boolean.TRUE);
+        assertNotNull(transferenciaReponseDto.getTransferenciaStatus());
     }
 
     @Test
     public void setTransferenciaStatus() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setTransferenciaStatus(TransferenciaStatus.PROCESSING_CONCLUDED);
+        assertEquals(TransferenciaStatus.PROCESSING_CONCLUDED,
+                transferenciaReponseDto.getTransferenciaStatus());
     }
 
     @Test
     public void getBancoDestino() {
-        assertTrue(Boolean.TRUE);
+        assertNotNull(transferenciaReponseDto.getBancoDestino());
     }
 
     @Test
     public void setBancoDestino() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setBancoDestino(null);
+        assertNull(transferenciaReponseDto.getBancoDestino());
+        transferenciaReponseDto.setBancoDestino(bancoDtoDestino);
+        assertNotNull(transferenciaReponseDto.getBancoDestino());
     }
 
     @Test
     public void getContaDestino() {
-        assertTrue(Boolean.TRUE);
+        assertEquals(STR, transferenciaReponseDto.getContaDestino());
     }
 
     @Test
     public void setContaDestino() {
-        assertTrue(Boolean.TRUE);
+        transferenciaReponseDto.setContaDestino(STR_ATUALIZADO);
+        assertEquals(STR_ATUALIZADO, transferenciaReponseDto.getContaDestino());
     }
 }

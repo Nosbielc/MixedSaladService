8d204221aa7d864fd9e70981209575b87729cb65
Cleibson Gomes da Silva <cleibson.gomes.da.silva@gmail.com>
2019-05-27 17:44:35 -0300

    Update CircuitBreakerConroller.java

--- banco-central-service/src/main/java/com/nosbielc/mixed/salad/bancocentral/controllers/CircuitBreakerConroller.java
+++ banco-central-service/src/main/java/com/nosbielc/mixed/salad/bancocentral/controllers/CircuitBreakerConroller.java
@@ -4,8 +4,6 @@ import com.nosbielc.mixed.salad.bancocentral.integration.CircuitBreakerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
 public class CircuitBreakerConroller {
 
     private static final Logger logger = LoggerFactory.getLogger(CircuitBreakerConroller.class);
@@ -21,14 +18,10 @@ public class CircuitBreakerConroller {
     @Autowired
     CircuitBreakerService circuitBreakerService;
 
-    @Value("${banco.central.circuitBreaker.enabled}")
-    private boolean circuitBreakerEnabled;
-
     @GetMapping(value = "/circuitBreaker", produces = "application/json", consumes = "application/json")
     @ResponseStatus(value = HttpStatus.OK)
     public ResponseEntity<String> circuitBreaker() {
         logger.info("circuitBreaker called");
-        logger.info("CircuitBreaker RefreshScope {}", circuitBreakerEnabled);
 
         return ResponseEntity.ok(this.circuitBreakerService.reading());
     }
